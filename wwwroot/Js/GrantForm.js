aa4881c27eea36de4015634091a311fac81d1f65
ma86jian <ma86jian@101f223c-5b42-2c59-c59f-9fe6e02cbb83>
2010-12-29 14:12:20 +0000

    added separate columns for edit, grants and delete added multiple selects for subareas rename css and js
    git-svn-id: https://chinafundseeker.googlecode.com/svn/trunk@42 101f223c-5b42-2c59-c59f-9fe6e02cbb83

--- wwwroot/Js/GrantForm.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-$(document).ready(function() {
-	$("input#submit").click(function() {
-		return Validate();
-	});
-});
--- wwwroot/Js/ListGrant.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-
-$(document).ready(function() {
-	$("a.actionDelete").click(function() {
-		return confirm("Are you sure to delete this Grant");
-	});
-});
